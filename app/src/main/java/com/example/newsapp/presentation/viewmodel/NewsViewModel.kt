f506f44e5ed62a075abf6132b3984060f3358364
Lucas Frota <lucv.frota@gmail.com>
2023-01-27 15:57:29 +0000

    feat: Create headline detais screen

--- app/src/main/java/com/example/newsapp/presentation/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/presentation/viewmodel/NewsViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.newsapp.data.repository.NewsRepository
+import com.example.newsapp.model.NewsData
 import com.example.newsapp.model.NewsResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -14,9 +15,14 @@ import javax.inject.Inject
 class NewsViewModel @Inject constructor(private val newsRepository: NewsRepository): ViewModel() {
 
     private var pageIndex = 1
+
     private var mutableHeadlines = MutableLiveData<NewsResponse>()
     val headlines: LiveData<NewsResponse>
         get() = mutableHeadlines
+
+    private var mutableHeadlineDetail = MutableLiveData<NewsData>()
+    val headLineDetail: LiveData<NewsData>
+        get() = mutableHeadlineDetail
 
     fun getHeadlines(sources: String, pageSize: Int){
         viewModelScope.launch {
@@ -25,6 +31,10 @@ class NewsViewModel @Inject constructor(private val newsRepository: NewsReposito
             }
         }
         pageIndex++
+    }
+
+    fun getHeadlineDetails(position: Int){
+        mutableHeadlineDetail.value = newsRepository.getCachedHeadlineDetails(position)
     }
 }
 
